27ea570001824425bd189bb4c1514d1be9df43d8
Vignesh <kamarthivignesh000@gmail.com>
2021-07-18 19:37:19 +0530

    Updated

--- project.py
+++ project.py
@@ -1,46 +1,42 @@
+'''Importing pytest'''
 import pytest
-
-def add(x, y):
-    return x + y
-    
-
-def subtract(x, y):
-    return x - y
-
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
-
-# This function divides two numbers
-def divide(x, y):
-    return x / y   
-
+'''Adding two numbers'''
+def add(num1, num2):
+    return num1+num2
+'''def sub():
+    Subtracting two numbers'''
+def subtract(num1, num2):
+    return num1 - num2
+'''def mul():
+    Multiplying two numbers'''
+def multiply(num1, num2):
+    return num1*num2
+'''def div():
+    Dividing two numbers'''
+def divide(num1, num2):
+    return num1/num2
+'''def square():
+    Squaring two numbers'''
 def square(num):
     return num * num
-
+'''def cube():
+    Cubing two numbers'''
 def cube(num):
     return num*num*num
-
 def squareroot(num):
     return num**0.5
-
-def factorial(n):
+def factorial(num):
     # single line to find factorial
-    return 1 if (n==1 or n==0) else n * factorial(n - 1)
-
+    return 1 if (num==1 or num==0) else num * factorial(num - 1)
 def decimal_to_binary(dec):
     decimal = int(dec)
-
+    return decimal
 def decimal_to_octal(dec):
     decimal = int(dec)
-
+    return decimal
 def decimal_to_hexadecimal(dec):
     decimal = int(dec)
-
-    
-
-    
-         
+    return decimal
 def calculate():
     operation = input('''
 Please type in the math operation you would like to complete:
@@ -56,134 +52,103 @@ Bin for decimal to binary
 Oct for decimal to octal
 Hex for decimal to Hexadecimal
 ''')
-
-    
-
     if operation == '+':
         num1 = int(input('Please enter the first number: '))
         num2 = int(input('Please enter the second number: '))
         print(num1, "+", num2, "=", add(num1, num2))
         output = str(add(num1,num2) )
-        f = open("demofile3.txt", "w+")
-        f.write(output)
-        f.close()
-
-
-
+        filename = open("demofile3.txt", "w+")
+        filename.write(output)
+        filename.close()
     elif operation == '-':
         num1 = int(input('Please enter the first number: '))
         num2 = int(input('Please enter the second number: '))
         print(num1, "-", num2, "=", subtract(num1, num2))
         output = str(subtract(num1,num2) )
-        f = open("demofile3.txt", "w+")
-        f.write(output)
-        f.close()
-
-
+        filename = open("demofile3.txt", "w+")
+        filename.write(output)
+        filename.close()
     elif operation == '*':
         num1 = int(input('Please enter the first number: '))
         num2 = int(input('Please enter the second number: '))
         print(num1, "*", num2, "=", multiply(num1, num2))
         output = str(multiply(num1,num2) )
-        f = open("demofile3.txt", "w+")
-        f.write(output)
-        
-        f.close()
-
-
+        filename = open("demofile3.txt", "w+")
+        filename.write(output)
+        filename.close()
     elif operation == '/':
         num1 = int(input('Please enter the first number: '))
         num2 = int(input('Please enter the second number: '))
         print(num1, "/", num2, "=", divide(num1, num2))
         output = str(divide(num1,num2) )
-        f = open("demofile3.txt", "w+")
-        f.write(output)
-        f.close()
-
+        filename = open("demofile3.txt", "w+")
+        filename.write(output)
+        filename.close()
     elif operation == 'sq':
         number = float(input("Please Enter any numeric Value : "))
         sqre = square(number)
         print("The Square of a Given Number {0}  = {1}".format(number, sqre))
         output = str(square(number) )
-        f = open("demofile3.txt", "w+")
-        f.write(output)
-        
-        f.close()
-
+        filename = open("demofile3.txt", "w+")
+        filename.write(output)
+        filename.close()
     elif operation == 'cube':
         number = float(input("Please Enter any numeric Value : "))
         cubes = cube(number)
         print("The Cube of a Given Number {0}  = {1}".format(number, cubes))
         output = str(cube(number) )
-        f = open("demofile3.txt", "w+")
-        f.write(output)
-        
-        f.close()
-
-
+        filename = open("demofile3.txt", "w+")
+        filename.write(output)
+        filename.close()
     elif operation == 'sqrt':
         number = float(input("Please Enter any numeric Value : "))
         sqrt = squareroot(number)
         print("The Square of a Given Number {0}  = {1}".format(number, sqrt))
         output = str(squareroot(number) )
-        f = open("demofile3.txt", "w+")
-        f.write(output)
-        
-        f.close() 
- 
+        filename = open("demofile3.txt", "w+")
+        filename.write(output)
+        filename.close()
     elif operation == 'fact':
         num = float(input("Please Enter any numeric Value : "))
         print ("Factorial of",num,"is",factorial(num))
         output = str(factorial(num) )
-        f = open("demofile3.txt", "w+")
-        f.write(output)
-        
-        f.close()
-
+        filename = open("demofile3.txt", "w+")
+        filename.write(output)
+        filename.close()
     elif operation == 'Bin':
         decimal = int(input("Enter any decimal number: "))
-        print(decimal, " in Binary : ", bin(decimal)) 
+        print(decimal, " in Binary : ", bin(decimal))
         output = str(bin(decimal) )
-        f = open("demofile3.txt", "w+")
-        f.write(output)
-        
-        f.close()
-
+        filename = open("demofile3.txt", "w+")
+        filename.write(output)
+        filename.close()
     elif operation == 'Oct':
         decimal = int(input("Enter any decimal number: "))
         print(decimal, "in Octal : ", oct(decimal))
         output = str(oct(decimal) )
-        f = open("demofile3.txt", "w+")
-        f.write(output)
-        
-        f.close() 
-
+        filename = open("demofile3.txt", "w+")
+        filename.write(output)
+        filename.close()
     elif operation == 'Hex':
         decimal = int(input("Enter any decimal number: "))
         print(decimal, " in Hexadecimal : ", hex(decimal))
         output = str(hex(decimal) )
-        f = open("demofile3.txt", "w+")
-        f.write(output)
-        
-        f.close() 
-
+        filename = open("demofile3.txt", "w+")
+        filename.write(output)
+        filename.close()
     else:
         print('You have not typed a valid operator, please run the program again.')
-
     # Add again() function to calculate() function
     again()
-
 def again():
     calc_again = input('''
 Please type Y for YES or N for NO.
 to continue
 ''')
-
     if calc_again.upper() == 'Y':
         calculate()
     elif calc_again.upper() == 'N':
         print('See you later.')
     else:
         again()
-
 calculate()
--- pytest.py
+++ pytest.py
@@ -1,61 +1,42 @@
 import pytest
 from project import add,decimal_to_hexadecimal,subtract,divide,multiply,square,cube,squareroot,factorial,decimal_to_binary,decimal_to_octal
-
 '''def calculate():
     a = '+'
     assert Python_project.calculate(a) == '+' '''
-
 def test_add():
-   a=2
-   b=9
-   assert add(a,b) == 11
-
-
+    num1=2
+    num2=9
+    assert add(num1,num2) == 11
 def test_subtract():
-   a=3
-   b=2
-   assert subtract(a,b) == 1
-
+    num1=3
+    num2=2
+    assert subtract(num1,num2) == 1
 def test_multiply():
-   a=5
-   b=2
-   assert multiply(a,b) == 10
-
+    num1=5
+    num2=2
+    assert multiply(num1,num2) == 10
 def test_divide():
-   a = 8
-   b = 2
-   assert divide(a,b) == 4
-
+    num1 = 8
+    num2 = 2
+    assert divide(num1,num2) == 4
 def test_square():
-   a = 8
-   
-   assert square(a) == 64
-
+    num = 8
+    assert square(num) == 64
 def test_cube():
-   a = 2
-   
-   assert cube(a) == 8
-
+    num = 2
+    assert cube(num) == 8
 def test_squareroot():
-   a = 16
-   
-   assert squareroot(a) == 4
-
+    num = 16
+    assert squareroot(num) == 4
 def test_factorial():
-   a = 5
-   
-   assert factorial(a) == 120
-
-
+    num = 5
+    assert factorial(num) == 120
 def test_decimal_to_binary():
-   dec = 8
-   assert bin(dec) == bin(8)
-
-
+    dec = 8
+    assert bin(dec) == bin(8)
 def test_decimal_to_octal():
-   dec =10
-   assert oct(dec) == oct(10)  
-
+    dec =10
+    assert oct(dec) == oct(10)
 def test_decimal_to_hexadecimal():
-   dec =10
-   assert hex(dec) == hex(10)       
+    dec =10
+    assert hex(dec) == hex(10)
